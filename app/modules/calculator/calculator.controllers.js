e510facb8fedcf9996b1498781bd318696e178a1
Carlos <shades3002@gmail.com>
2017-05-18 22:49:56 -0400

    controller -calculator logical

--- app/modules/calculator/calculator.controllers.js
+++ app/modules/calculator/calculator.controllers.js
@@ -4,12 +4,59 @@
         .module('app.calculator.controllers')
         .controller('calculatorCtrl', calculatorCtrl);
 
-    //calculatorCtrl.$inject = [];
+    calculatorCtrl.$inject = ['$scope'];
 
-    function calculatorCtrl() {
+    function calculatorCtrl($scope) {
         /* jshint validthis: true */
         var vm = this;
+        vm.getCalculator = getCalculator;
+        vm.init = init;
+        vm.calc = {};
+        vm.init();
 
-        vm.mensage = "Welcome";
+        $scope.$watch(function () { 
+                return vm.calc; 
+            }, 
+            function(New, Old) {
+                if (New === Old) {
+                    return;
+                } else {
+                    vm.getCalculator();
+                }
+        }, true);
+
+        function getCalculator() {
+
+            if(vm.calc.percentage > 0) {
+                var percentage = 1 - (vm.calc.percentage / 100);
+            }
+
+            if ( vm.calc.SToReceive !== undefined && vm.calc.SToReceive !== '' && vm.calc.SToReceive !== null) {
+                vm.calc.SToSend = Math.round(((vm.calc.SToReceive + vm.calc.commission) / percentage) * 100) / 100;
+                vm.calc.SToComision = Math.round((vm.calc.SToSend - vm.calc.SToReceive) * 100) / 100;  
+            } else {
+                vm.calc.SToSend = vm.calc.SToComision = ''; 
+            }
+
+            if ( vm.calc.RToReceive !== undefined && vm.calc.RToReceive !== '' && vm.calc.RToReceive !== null) {
+                vm.calc.RToSend = Math.round((vm.calc.RToReceive * percentage - vm.calc.commission) * 100) / 100; 
+                vm.calc.RToComision = Math.round((vm.calc.RToReceive - vm.calc.RToSend) * 100) / 100;               
+            } else {
+                vm.calc.RToSend = vm.calc.RToComision = ''; 
+            }
+        }
+
+        function init() {
+            vm.calc = {
+                percentage: 5.4, 
+                commission: 0.30,
+                SToReceive: '',
+                SToSend: '',
+                SToComision: '',
+                RToReceive: '',
+                RToSend: '',
+                RToComision: ''
+            };
+        }
     }
 })();
